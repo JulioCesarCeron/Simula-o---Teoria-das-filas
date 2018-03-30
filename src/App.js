1593eecaff166918e8fb6f5f6a670d7e0074b36a
Julio Cesar Ceron <julio.ceorn@gmail.com>
2018-03-29 22:14:10 -0300

    corrigido link de deploy e removido trecho comentado no render

--- src/App.js
+++ src/App.js
@@ -304,29 +304,6 @@ class App extends Component {
                                         <th scope="col">C</th>
                                     </tr>
                                 </thead>
-                                {/* <tbody>
-                                    <tr>
-                                        <th scope="row">1</th>
-                                        <td>Mark</td>
-                                        <td>Otto</td>
-                                        <td>@mdo</td>
-                                        <td>@mdo</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">2</th>
-                                        <td>Jacob</td>
-                                        <td>Thornton</td>
-                                        <td>@fat</td>
-                                        <td>@mdo</td>
-                                    </tr>
-                                    <tr>
-                                        <th scope="row">3</th>
-                                        <td>Larry</td>
-                                        <td>the Bird</td>
-                                        <td>@twitter</td>
-                                        <td>@mdo</td>
-                                    </tr>
-                                </tbody> */}
                                 {table_content}
                             </table>
                         </div>
